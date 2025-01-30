fe34e8b4fd4571c1f280526da3900b0d33511b18
pratham182 <arorapratham900@gmail.com>
2025-01-30 11:29:03 +0530

    commit

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -1,51 +1,32 @@
-import React, { useContext, useEffect } from 'react';
-import { Link} from 'react-router-dom';
+import React from 'react';
+import { useNavigate } from 'react-router-dom';  // Import the useNavigate hook
 import '../styles/Navbar.css';
 import { logout } from '../api';
 
-
-
-
-
 const Navbar = () => {
- 
+  const navigate = useNavigate();  
+
   const handleLogout = async () => {
     try {
       const result = await logout();  
 
-      console.log(result);  
-     
+      console.log(result);
 
       if (result.success) {
-        window.location.href = "/";
-       
+        navigate('/');  
       } else {
-        console.error("Logout failed", result.message);  
+        console.error("Logout failed", result.message);
       }
     } catch (error) {
-      console.error("Error during logout:", error);  
+      console.error("Error during logout:", error);
     }
   };
 
-  
   return (
     <nav className="navbar">
       <h1>Bank Info System</h1>
       <div>
-
-        
-
-     
-      
-
-
-        
-          
-         <button onClick={handleLogout}>Logout</button>
-      
-         
-         
-        
+        <button onClick={handleLogout}>Logout</button>
       </div>
     </nav>
   );
