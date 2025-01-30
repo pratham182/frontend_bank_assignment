e7bbe76d3b865b53d3c6a83b486cf4b2139f8a63
pratham182 <arorapratham900@gmail.com>
2025-01-30 11:23:50 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,11 @@ import AdminLogin from './pages/AdminLogin';
 import AdminDashboard from './pages/AdminDashboard';
 import AdminRoute from './Component/AdminRoute'; 
 
-const Buttons = () => {
-  return (
-    
-      <div style={styles.divbar}>
-        <h2 style={styles.logo}>Portal</h2>
-        <div>
-          <Link to="/login" style={styles.link}>User Login</Link>
-          <Link to="/register" style={styles.link}>Register</Link>
-          <Link to="/adminLogin" style={styles.link}>Admin Login</Link>
-        </div>
-      </div>
-  )
-};
+
 const App = () => {
   return (
     <Router>
-      <Buttons/>
+      
       <Routes>
         <Route path="/login" element={<AuthRoute isPublic={true} element={<Login />} />} />
         <Route path="/register" element={<AuthRoute isPublic={true} element={<Register />} />} />
@@ -40,27 +28,6 @@ const App = () => {
 };
 
 
-const styles = {
-  divbar: {
-    display: "flex",
-    justifyContent: "space-between",
-    alignItems: "center",
-    padding: "10px 20px",
-    backgroundColor: "#007bff",
-    color: "white",
-  },
-  logo: {
-    margin: 0,
-  },
-  link: {
-    margin: "0 10px",
-    color: "white",
-    textDecoration: "none",
-    fontSize: "16px",
-    padding: "5px 10px",
-    borderRadius: "5px",
-    backgroundColor: "#0056b3",
-  },
-};
+
 
 export default App;
